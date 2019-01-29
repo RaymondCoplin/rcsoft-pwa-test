b3a1d829fdaf8196cb845bc1b1c436de4a89cb7a
Raymond Coplin <raymondcoplin@gmail.com>
2019-01-29 17:33:22 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+(function () {
+  var burger = document.querySelector('.burger');
+  var nav = document.querySelector('#' + burger.dataset.target);
+
+  burger.addEventListener('click', function () {
+    burger.classList.toggle('is-active');
+    nav.classList.toggle('is-active');
+  });
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('./service-worker.js')
+      .then(function () {
+        console.log('Service Worker Registered...')
+      });
+  }
+
+})();
---
+++ service-worker.js
@@ -0,0 +1,44 @@
+var dataCacheName = 'weatherData-v1';
+var cacheName = 'rcsoft-pwa-v1-1';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/index.js',
+  '/assets/logo.png',
+  '/assets/raymondcoplin.jpg',
+  '/assets/business-developer.jpg',
+  'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.css'
+];
+
+self.addEventListener('install', e => {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', e => {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(keyList => { 
+      return Promise.all(keyList.map(key => {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }))
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  )
+});
